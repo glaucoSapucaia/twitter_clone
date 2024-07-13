dc13d366ced36c27f43e2432a7f422b2439e6d48
glaucoSapucaia <glaucopco@gmail.com>
2024-07-13 11:53:26 -0300

    Validando e salvando dados dos usuarios | Usuario-> validarCadastro(), getUsuarioPorEmail() | Fluxo de erro no cadastro | inscreverse.phtml e cadastro.phtml | indexController-> registrar()

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -14,6 +14,16 @@ class IndexController extends Action {
 	}
 
 	public function inscreverse() {
+		// Mantendo dados vazios nos campos (erro ao renderizar a pagina devido a recuperação dos 
+		// dados fornecidos pelo usuario em caso de erro)
+		$this->view->usuario = array(
+			'nome' => '',
+			'email' => '',
+			'senha' => '',
+		);
+
+		// definindo fluxo para diferentes chamadas da view inscreverse
+		$this->view->erroCadastro = false;
 
 		$this->render('inscreverse');
 	}
@@ -24,16 +34,50 @@ class IndexController extends Action {
 		// 	print_r($_POST);
 		// echo '</pre>';
 
+		// Criando classe dinamica e estabelecendo conexao com DB
 		$usuario = Container::getModel('Usuario');
 
+		// Setando attrs de Usuario obj
 		$usuario->__set('nome', $_POST['nome']);
 		$usuario->__set('email', $_POST['email']);
 		$usuario->__set('senha', $_POST['senha']);
 
 		// debug
-		echo '<pre>';
-			print_r($usuario);
-		echo '</pre>';
+		// echo '<pre>';
+		// 	print_r($usuario);
+		// echo '</pre>';
+
+		// Validando e salvando dados no db | Usuario obj
+		if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) {
+			// Verificando se usuario ja existe
+
+			// debug
+			// echo '<pre>';
+			// 	print_r($usuario->getUsuarioPorEmail());
+			// echo '</pre>';
+
+			// Se maior que 0, já existe!
+			$usuario->salvar();
+
+			// renderizando view de sucesso do cadastro
+			$this->render('cadastro');
+
+		} else {
+			// Fluxo para erro no cadastro
+
+			// mantendo informações adicionadas pelo usuario no redirecionamento da pagina
+			$this->view->usuario = array(
+				'nome' => $_POST['nome'],
+				'email' => $_POST['email'],
+				'senha' => $_POST['senha'],
+			);
+
+			// parametro que indica erros no cadastro
+			$this->view->erroCadastro = true;
+
+			$this->render('inscreverse');
+		}
+		
 	}
 
 }
--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -44,5 +44,45 @@
 
             return $this;
         }
+
+        // validando dados de cadastro
+        public function validarCadastro() {
+            // flag para validação
+            $valido = true;
+
+            // Verificando se campos possuem pelo menos 3 caracteres
+            if(strlen($this->__get('nome')) < 3) {
+                $valido = false;
+            }
+
+            if(strlen($this->__get('email')) < 3) {
+                $valido = false;
+            }
+
+            if(strlen($this->__get('senha')) < 3) {
+                $valido = false;
+            }
+
+            return $valido;
+        }
+
+        // resgatando dados para não permitir registros duplicados
+        public function getUsuarioPorEmail() {
+            $query = '
+                select
+                    nome, email
+                from
+                    usuarios
+                where
+                    email = :email
+            ';
+
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':email', $this->__get('email'));
+            $stmt->execute();
+
+            // FETCH_ASSOC -> Retorna um array() associativo
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        }
     }
 ?>
